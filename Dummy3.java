30d7763ef626b4ccd8df82a9307448c8d966631c
vishnu vardhan <vishnuvvr3@gmail.com>
2025-11-04 18:35:23 +0530

    commit for this message new commit for conflict

--- Dummy3.java
+++ Dummy3.java
@@ -3,6 +3,8 @@ public class Dummy3{
 
     public void dummyMethod() {
          System.out.println("has Value");
+         System.out.println("good value");
+         System.out.println("has Value");
          System.out.println("no value");
     }
 }
