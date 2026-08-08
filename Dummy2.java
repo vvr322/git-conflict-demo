e27902b11c263e19f2864addcb739c6a375a01f1
vishnu vardhan <vishnuvvr3@gmail.com>
2026-08-08 17:25:07 +0530

    local v2: cookies

--- Dummy2.java
+++ Dummy2.java
@@ -2,6 +2,6 @@ public class Dummy2 {
 
     public void addMethod()
     {
-        System.out.println("LOCAL: sahithi eating ice cream");
+        System.out.println("LOCAL v2: sahithi eating cookies");
     }
 }
